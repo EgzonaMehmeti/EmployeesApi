335140774ec1f4e336d3600f01729a536828eae1
EgzonaMehmeti <146218991+EgzonaMehmeti@users.noreply.github.com>
2026-01-11 18:17:40 +0100

    smth

--- obj/Debug/net8.0/EmployeesApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144bc63bce3da2159f058100bf8c24f9e973fcd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
